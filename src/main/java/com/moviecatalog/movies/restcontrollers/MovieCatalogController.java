b7c908ecbc08612c299dbef9e52971bebf3696fd
tcm84 <thomascmooney@hotmail.co.uk>
2018-08-19 23:41:48 +0100

    Updated javadocs

--- src/main/java/com/moviecatalog/movies/restcontrollers/MovieCatalogController.java
+++ src/main/java/com/moviecatalog/movies/restcontrollers/MovieCatalogController.java
@@ -9,38 +9,22 @@ import com.moviecatalog.movies.model.MovieDetails;
 */
 public interface MovieCatalogController {
 
-	/**
-	* Adds a new movie to this catalog with these 
-	* movieDetails
-	*
-	* @param moviesDetails of the new movie
-	* @return An instance of MoviesDetails for the movie just 
-	* added to this catalog including the generated movieId
-	* @exception Throws MovieExistsException if a movie already
-	* exists in this catalog with these details
+	/** 
+	* @see MovieCatalogService#addToCatalog(MovieDetails)
 	* @exception Throws MethodArgumentNotValidException if movieDetails
 	* fails the validation requirements
 	*/
 	MovieDetails addToCatalog(MovieDetails movieDetails);
 
 	/**
-	* Updates an existing movie in this catalog with these 
-	* movieDetails
-	*
-	* @param updated moviesDetails for an existing movie 
-	* @return An instance of MoviesDetails for the movie just 
-	* updated
-	* @exception Throws MovieNotFoundException the movie with 
-	* this movieId does not exist in this catalog
+	* @see MovieCatalogService#updateCatalog(MovieDetails)
 	* @exception Throws MethodArgumentNotValidException if movieDetails
 	* fails the validation requirements
 	*/
 	MovieDetails updateCatalog(MovieDetails movieDetails);
 
 	/**
-	* Deletes a movie from this catalog with this movieId
-	*
-	* @param movieId of the movie to delete from this catalog
+	* @see MovieCatalogService#deleteFromCatalog(Integer)
 	*/
 	void deleteFromCatalog(Integer movieId);
 }
--- src/main/java/com/moviecatalog/movies/services/MovieCatalogService.java
+++ src/main/java/com/moviecatalog/movies/services/MovieCatalogService.java
@@ -8,10 +8,35 @@ import com.moviecatalog.movies.model.MovieDetails;
 * on a MovieCatalogRepository
 */
 public interface MovieCatalogService {
-
+	
+	/**
+	* Adds a new movie to this catalog with these 
+	* movieDetails
+	*
+	* @param moviesDetails of the new movie
+	* @return An instance of MoviesDetails for the movie just 
+	* added to this catalog including the generated movieId
+	* @exception Throws MovieExistsException if a movie already
+	* exists in this catalog with these details
+	*/
 	MovieDetails addToCatalog(MovieDetails movieDetails);
 
+	/**
+	* Updates an existing movie in this catalog with these 
+	* movieDetails
+	*
+	* @param updated moviesDetails for an existing movie 
+	* @return An instance of MoviesDetails for the movie just 
+	* updated
+	* @exception Throws MovieNotFoundException the movie with 
+	* this movieId does not exist in this catalog
+	*/
 	MovieDetails updateCatalog(MovieDetails movieDetails);
 
+	/**
+	* Deletes a movie from this catalog with this movieId
+	*
+	* @param movieId of the movie to delete from this catalog
+	*/
 	void deleteFromCatalog(Integer movieId);
 }
