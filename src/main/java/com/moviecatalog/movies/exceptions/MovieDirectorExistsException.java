849e89555ef3eab7e8a454718e31b76eda48dbc4
tcm84 <thomascmooney@hotmail.co.uk>
2018-08-20 23:57:07 +0100

    Commiting missed Exception types

---
+++ src/main/java/com/moviecatalog/movies/exceptions/MovieDirectorExistsException.java
@@ -0,0 +1,8 @@
+package com.moviecatalog.movies.exceptions;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ResponseStatus(value=HttpStatus.CONFLICT, reason="MovieDirector already exists in the catalog")
+public class MovieDirectorExistsException extends RuntimeException{
+}
