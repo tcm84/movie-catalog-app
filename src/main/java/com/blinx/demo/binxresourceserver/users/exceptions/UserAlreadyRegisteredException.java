3bc6beca00e89e2c4a3060b6b8b5a31c4de01873
tcm84 <thomascmooney@hotmail.co.uk>
2018-08-21 03:14:31 +0100

    Removing the blinx commits that somehow got in there

--- src/main/java/com/blinx/demo/binxresourceserver/users/exceptions/UserAlreadyRegisteredException.java
+++
@@ -1,9 +0,0 @@
-package com.blinx.demo.binxresourceserver.users.exceptions;
-
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.http.HttpStatus;
-
-@ResponseStatus(value=HttpStatus.CONFLICT, reason="User has already registered")
-public class UserAlreadyRegisteredException extends RuntimeException{
-
-}
